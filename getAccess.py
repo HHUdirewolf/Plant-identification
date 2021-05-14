15a198697cf59dd9ec334ead71b99a4a1c85bf1a
HHUdirewolf <43950544+HHUdirewolf@users.noreply.github.com>
2021-05-14 23:17:14 +0800

    Update getAccess.py

--- getAccess.py
+++ getAccess.py
@@ -1,7 +1,7 @@
 #获取access_key
 import urllib.request
-API_key="PNKnBEO4cL21QfmMYChxaE14"
-SECRET_KEY="QWF38qheMsWXGz4GgfWY7SUwW2CWhUKc"
+API_key="yours"#输入你的api_key
+SECRET_KEY="yours"
 host="https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id="+API_key+"&client_secret="+SECRET_KEY
 request=urllib.request.Request(host)
 request.add_header('Content-Type','application/json;charset=utf-8')
